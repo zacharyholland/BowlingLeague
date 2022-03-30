55f9d9ba7542dbab6a28607afb509709d71ee367
zacharyholland <zachary.lyman.holland@gmail.com>
2022-03-30 15:50:40 -0600

    final commit

--- BowlingLeague/Controllers/HomeController.cs
+++ BowlingLeague/Controllers/HomeController.cs
@@ -25,5 +25,55 @@ namespace BowlingLeague.Controllers
 
             return View(x);
         }
+
+        [HttpGet]
+        public IActionResult CreateBowler()
+        {
+            //brings in teams list
+            ViewBag.Teams = _repo.Teams.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBowler(Bowler b)
+        {
+
+            if (ModelState.IsValid)
+            {
+                b.BowlerID = (_repo.Bowlers.Max(b => b.BowlerID)) + 1;
+                _repo.CreateBowler(b);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                ViewBag.Teams = _repo.Teams.ToList();
+                return View(b);
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int bowlerid)
+        {
+
+            ViewBag.Teams = _repo.Teams.ToList();
+            var bowler = _repo.Bowlers.Single(x => x.BowlerID == bowlerid);
+            return View("CreateBowler", bowler);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler update)
+        {
+            _repo.SaveBowler(update);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(Bowler delete)
+        {
+            _repo.DeleteBowler(delete);
+
+            return RedirectToAction("Index");
+        }
     }
 }
